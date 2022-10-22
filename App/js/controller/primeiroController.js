7417de139ca542c3597ee76aca76182b9a2b032c
Paulo <paulocavalcante001@outlook.com>
2022-10-22 19:13:29 -0300

    http

--- App/js/controller/primeiroController.js
+++ App/js/controller/primeiroController.js
@@ -1,30 +1,31 @@
-app.controller('primeiroController', ['$scope', function($scope) {
-    $scope.user = {meuNome: 'Paulo', meuSobrenome: 'Cavalcante'};
+app.controller("primeiroController", [
+  "$scope",
+  function ($scope) {
+    $scope.user = { meuNome: "Paulo", meuSobrenome: "Cavalcante" };
 
     $scope.contador = 0;
 
-    $scope.addAcontador = function() {
-        console.log(document.getElementById('corpo'));
-        $scope.contador++;
-    }
+    $scope.addAcontador = function () {
+      console.log(document.getElementById("corpo"));
+      $scope.contador++;
+    };
 
-    $scope.pessoas = ['ana', 'mario', 'josé'];
-}]);
+    $scope.pessoas = ["ana", "mario", "josé"];
+  },
+]);
 
-app.controller('localizacao', ['$scope', '$location', function($scope, $location) {
-    $scope.myUrl = $location.absUrl();
-}]);
-
-app.controller('controllerTimeOut', ['$scope', '$timeout', function($scope, $timeout) {
-    $scope.timermsg = "Oi!";
-    $timeout(function() {
-        $scope.timermsg = "Oi depois de 3 segundos";
-    }, 3000)
-}]);
-
-app.controller('controllerIntervalo', ['$scope', '$interval', function($scope, $interval) {
-    $scope.intervalo = new Date().toLocaleTimeString();
-    $interval(function() {
-        $scope.intervalo = new Date().toLocaleTimeString();
-    }, 1000)
-}]);
+app.controller("pegarResposta", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.pegarResposta = function () {
+      $http
+        .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+        .then(function (response) {
+            $scope.lista = response.data;
+        }).catch(function(e) {
+            console.log('falhou');
+        });
+    };
+  },
+]);
