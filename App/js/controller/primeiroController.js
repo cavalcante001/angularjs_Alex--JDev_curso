263e5361a28cb486fac1af9015bbb4f635639c01
Paulo <paulocavalcante001@outlook.com>
2022-10-22 10:35:26 -0300

    data bind, controller, ngRepeat, ngInit, scope

---
+++ App/js/controller/primeiroController.js
@@ -0,0 +1,12 @@
+angular.module('loja').controller('primeiroController', ['$scope', function($scope) {
+    $scope.user = {meuNome: 'Paulo', meuSobrenome: 'Cavalcante'};
+
+    $scope.contador = 0;
+
+    $scope.addAcontador = function() {
+        console.log(document.getElementById('corpo'));
+        $scope.contador++;
+    }
+
+    $scope.pessoas = ['ana', 'mario', 'josé'];
+}]);
