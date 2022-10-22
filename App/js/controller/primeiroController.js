7226aa259762b4b9ff22900d24ad8ff0ede97b64
Paulo <paulocavalcante001@outlook.com>
2022-10-22 18:52:24 -0300

    services

--- App/js/controller/primeiroController.js
+++ App/js/controller/primeiroController.js
@@ -10,3 +10,21 @@ app.controller('primeiroController', ['$scope', function($scope) {
 
     $scope.pessoas = ['ana', 'mario', 'josé'];
 }]);
+
+app.controller('localizacao', ['$scope', '$location', function($scope, $location) {
+    $scope.myUrl = $location.absUrl();
+}]);
+
+app.controller('controllerTimeOut', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.timermsg = "Oi!";
+    $timeout(function() {
+        $scope.timermsg = "Oi depois de 3 segundos";
+    }, 3000)
+}]);
+
+app.controller('controllerIntervalo', ['$scope', '$interval', function($scope, $interval) {
+    $scope.intervalo = new Date().toLocaleTimeString();
+    $interval(function() {
+        $scope.intervalo = new Date().toLocaleTimeString();
+    }, 1000)
+}]);
